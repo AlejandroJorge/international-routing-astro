03c57667c24368acb8508bdc9be4fa38fced4a74
AlejandroJorge <a20202060@pucp.edu.pe>
2024-03-11 10:32:22 -0500

    Fixed internationalization for content collections

--- src/lib/routing.ts
+++ src/lib/routing.ts
@@ -1,15 +1,8 @@
-function removeLang(seq: string, starting: number) {
-	const index = seq.indexOf("/", starting);
-	if (index === -1) {
+export function removeFirstPathPart(path: string) {
+	const startingIndex = path.startsWith("/") ? 1 : 0;
+	const cutIndex = path.indexOf("/", startingIndex);
+	if (cutIndex === -1) {
 		return "/";
 	}
-	return seq.slice(index);
-}
-
-export function removeLangFromPath(path: string) {
-	return removeLang(path, 1);
-}
-
-export function removeLangFromSlug(slug: string) {
-	return removeLang(slug, 0);
+	return path.slice(cutIndex);
 }
